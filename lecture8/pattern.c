fa2546683e36b032f9a28e7ab15f41744c893d3c
abhijeetbaghel777 <abhijeetbaghel77@gmail.com>
2025-09-15 17:28:40 +0530

    pattern

---
+++ lecture8/pattern.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    for(int i=0;i<=4;i++){
+        for(int j=0;j<=4;j++){
+            if (i==j || j==(4-i))
+            {
+                printf("*");
+                /* code */
+            }else{
+                printf(" ");
+            }
+            
+        }
+        printf("\n");
+    }
+}
