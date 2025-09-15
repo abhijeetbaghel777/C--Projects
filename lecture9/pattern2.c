455d1b9dd87b9a0bc7faf4ad3c79bad1357f9189
abhijeetbaghel777 <abhijeetbaghel77@gmail.com>
2025-09-15 17:29:51 +0530

    pattern

---
+++ lecture9/pattern2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    for(int i=0;i<4;i++){
+
+           for(int j=0;j<=i;j++){
+               printf(" ");
+               }
+              for(int k=3;k>i;k--){
+                printf("*");
+               }
+                 for(int p=3;p>=i;p--){
+                printf("*");
+               }
+               printf("\n");
+
+        }
+    }
