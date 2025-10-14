8727afa25e836d430b92c3a789b7f8b42f36ccf5
abhijeetbaghel777 <abhijeetbaghel77@gmail.com>
2025-10-14 19:50:28 +0530

    array

---
+++ lecture13/array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    int a=100;
+    int b=101;
+    int arr[5]={1};
+    int max=arr[0];
+    for(int i=0;i<4;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+       
+    }
+    printf("%d",max);
+    printf("%d",arr[45]);
+    for(int i=2;i>=0;i--){
+        printf("%d",arr[i]);
+    }
+
+}
---
+++ lecture13/target.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+      int arr[5]={3,12,52,34,5};
+int target=3;
+for(int i =0;i<5;i++){
+    if(arr[i]==target){
+        printf("%d",arr[i]);
+        break;
+    }
+}
+
+}
+
+  
